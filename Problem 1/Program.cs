e4675ee3e85f697be891cd1ffb19011b4bd10a5b
Berik Bakhitov <bakhitov2001@mail.ru>
2019-02-15 20:34:26 +0600

    Ya Batya

---
+++ Problem 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program {
+    public static void Main() {
+        int z = int.Parse (Console.ReadLine());                                                   //Количество элементов
+        int[] m = Array.ConvertAll<string, int> (Console.ReadLine().Split(), int.Parse);         // Создал массив
+        int[] Prime = new int[z];                                                               // Создал массив для простых чисел
+        int cnt = 0;                                                                           // Создал счетчик для простых чисел
+        for(int i = 0; i < z; i++){                                                           // Пробегаюсь в цикле по всем элементам массива чисе
+            bool tf = true;                                                                  // Создал флаг, отвечающий за добавление в Prime
+            for(int j = 2; j < m[i]; j++) {                                                 // Проверка числа на простое или сложное
+                if(m[i] % j == 0) {
+                    tf = false;
+                    break;
+                }
+            }
+            if(tf == true && m[i] != 1) {                                             // Если флаг tf == true и не равняется одному, то оно добавляется Prime числа
+                Prime[cnt] = m[i];
+                cnt++;
+            }
+        }
+        
+        Console.WriteLine(cnt);
+        for(int i = 0; i < cnt; i++) {
+            Console.Write(Prime[i] + " ");
+        }
+    }
+}
+
+
+
+
---
+++ Problem 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2 {
+    class Program {
+        static void Main(string[] args) {
+            Student stu = new Student("Bakhitov Berik", 12345678);
+            stu.Pozvatb();
+        }
+    }
+
+    class Student {
+        string name;
+        int id;
+        int year;
+
+        public Student(string name, int id) {
+            this.name = name;
+            this.id = id;
+            year = 2019;
+        }
+
+        public void Pozvatb() {
+            Console.WriteLine("Klichka: {0} Id: {1}", name, id);
+            year++;
+        }
+    }   
+}
